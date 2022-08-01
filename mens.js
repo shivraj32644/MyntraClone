c0ee1c6628a7986716c374f7d557afe76fad254a
shivraj32644 <shivraj32644@gmail.com>
2022-08-01 16:32:10 +0530

    products

---
+++ mens.js
@@ -0,0 +1,501 @@
+var mensData = [
+    {
+        image_url: "image/img1.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img2.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img3.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img4.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img5.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img6.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img7.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img8.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img9.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img10.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img11.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img12.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img13.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img14.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img15.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img16.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img17.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img18.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img19.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img20.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img21.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img22.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img23.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img24.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img25.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img26.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img27.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img28.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img29.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img30.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img31.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img32.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img33.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img34.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img35.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img36.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img37.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img38.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img39.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img40.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img41.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img42.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img43.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img44.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img45.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img46.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img47.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img48.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img49.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    {
+        image_url: "image/img50.webp",
+        brand:"Allen Soley",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        size:"38,40,41,42,43",
+        price: "1170",
+        strikedoffprice: "2599",
+      
+    },
+    
+    
+  ];
+ 
+  mensData.map(function (element, index) {
+    var boxdiv = document.createElement("div");
+    var img = document.createElement("img");
+    var name = document.createElement("p");
+    var Pricediv = document.createElement("div");
+    var Dis_price = document.createElement("p");
+    var Ori_price = document.createElement("p");
+    var btn = document.createElement("button");
+
+    img.setAttribute("src", element.image_url);
+    name.innerText = element.name;
+    Dis_price.innerText = "₹ " + element.price;
+    Ori_price.innerText = element.strikedoffprice;
+
+    var cartLogo = document.createElement("img");
+    cartLogo.setAttribute(
+      "src",
+      "https://cdn-icons-png.flaticon.com/512/891/891462.png"
+    );
+    var cartText = document.createElement("p");
+    cartText.innerText = "Add to Cart";
+    btn.appendChild(cartLogo);
+    btn.appendChild(cartText);
+
+    boxdiv.setAttribute("id", "sub-box");
+    Pricediv.setAttribute("id", "price-box");
+    btn.setAttribute("id", "btn");
+    name.setAttribute("id", "name");
+
+    btn.addEventListener("click", function () {
+      //
+      addToCart(index);
+    });
+
+    Pricediv.append(Dis_price, Ori_price);
+    boxdiv.append(img, name, Pricediv, btn);
+    document.getElementById("main-box").append(boxdiv);
+  });
+  var McartArr = JSON.parse(localStorage.getItem("McartData")) || [];
+  function addToCart(i) {
+    var product = mensData.filter(function (elem, index) {
+      return index == i;
+    });
+    McartArr.push(product[0]);
+    localStorage.setItem("McartData", JSON.stringify(McartArr));
+  }
